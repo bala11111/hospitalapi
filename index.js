89755481ebcd44facbec0923e8c0d8908f36748e
bala11111 <balki.98@gmail.com>
2020-09-05 23:53:11 +0530

    added mongoose

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const express = require('express');
 const app = express();
 const port = 8000;
 
+const db = require('./config/mongoose');
+app.use(express.urlencoded());
 
 app.listen(port,function(err){
     if(err)
